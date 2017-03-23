2297434ca22e5dc212df45cd5525439edc77cc2e
Django Ruppmann <druppmann@me.com>
2017-03-23 12:21:42 +0700

    challenge commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -30,6 +31,11 @@ class ViewController: UIViewController {
         messageLabel.isHidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.red
+        
+        nameLabel.isHidden = false
+        nameLabel.text = enterNameTextField.text
+        enterNameTextField.text = ""
+        nameLabel.textColor = UIColor.blue
         
         enterNameTextField.text = ""
         enterMessageTextField.resignFirstResponder()
